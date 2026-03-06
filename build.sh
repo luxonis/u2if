17686134b37dc557569a1c6541dd4f3108d4a169
stas.bucik <stas.bucik@luxonis.com>
2026-03-06 20:25:37 +0100

    Add docker build environment and helper scripts
    Signed-off-by: stas.bucik <stas.bucik@luxonis.com>

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+pushd docker
+  docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t u2if-builder .
+popd
+
+docker run --rm -it -v "$PWD":/work u2if-builder bash build_in_container.sh
--- build_in_container.sh
+++ build_in_container.sh
@@ -1,22 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
 git submodule update --init --recursive
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-rm -rf ${DIR}/firmware/source/build 
-mkdir ${DIR}/firmware/source/build
+if [ -e ${DIR}/firmware/source/build ]; then
+  rm -rf ${DIR}/firmware/source/build
+  mkdir ${DIR}/firmware/source/build
+fi
 cd ${DIR}/firmware/source/build
 
-rm ./u2if_CUSTOM_1V8.uf2
+if [ -e ./u2if_CUSTOM_1V8.uf2 ]; then
+  rm ./u2if_CUSTOM_1V8.uf2
+fi
 cmake .. -DBOARD=CUSTOM_1V8 -DIS_1V8=1
 make -j$(nproc --all)
 cp ./u2if_CUSTOM_1V8.uf2 ${DIR}/dist/u2if_1v8.uf2
 
-rm ./u2if_CUSTOM_3V3.uf2
+if [ -e ./u2if_CUSTOM_3V3.uf2 ]; then
+  rm ./u2if_CUSTOM_3V3.uf2
+fi
 cmake .. -DBOARD=CUSTOM_3V3 -DIS_1V8=0
 make -j$(nproc --all)
 cp ./u2if_CUSTOM_3V3.uf2 ${DIR}/dist/u2if_3v3.uf2
 
-rm ./u2if_SL6945_1V8.uf2
+if [ -e ./u2if_SL6945_1V8.uf2 ]; then
+  rm ./u2if_SL6945_1V8.uf2
+fi
 cmake .. -DBOARD=SL6945_1V8 -DIS_1V8=1
 make -j$(nproc --all)
 cp ./u2if_SL6945_1V8.uf2 ${DIR}/dist
