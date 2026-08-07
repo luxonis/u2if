7ab153ca57eea2bbe0b273f8d83ebe71ff012a4e
Amadej <amadej.arh@luxonis.com>
2026-08-07 17:04:00 +0200

    added fsync interface, interface needs testing

--- firmware/source/interfaces/FsyncController.cpp
+++ firmware/source/interfaces/FsyncController.cpp
@@ -3,6 +3,10 @@
 #include "hardware/i2c.h"
 #include "pico/stdlib.h"
 
+#include <limits>
+#include <cstring>
+#include <math.h>
+
 FsyncController::FsyncController()
 {
 
@@ -17,29 +21,35 @@ CmdStatus FsyncController::process(uint8_t const *cmd, uint8_t response[64])
 {
     CmdStatus status = CmdStatus::NOT_CONCERNED;
 
-    if(cmd[0] == Report::ID::FSYNC_INIT) {
-        status = Init(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_PROBE) {
+    if (Report::ID::FSYNC_GETPINCAPABILITIES <= cmd[0] && cmd[0] <= Report::ID::FSYNC_GETPOLARITY && locked)
+        unlockFsyncController(); 
+
+    if (Report::ID::FSYNC_GETMODE <= cmd[0] && cmd[0] <= Report::ID::FSYNC_GETPOLARITY && !initok)
+        return CmdStatus::NOK;
+
+    if (cmd[0] == Report::ID::FSYNC_INIT) {
+        status = init(cmd, response);
+    } else if (cmd[0] == Report::ID::FSYNC_PROBE) {
         status = probe(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_GETPINCAPABILITIES) {
+    } else if (cmd[0] == Report::ID::FSYNC_GETPINCAPABILITIES) {
         status = getPinCapabilities(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_SETPINCAPABILITIES) {
+    } else if (cmd[0] == Report::ID::FSYNC_SETPINCAPABILITIES) {
         status = setPinCapabilities(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_GETMODE) {
+    } else if (cmd[0] == Report::ID::FSYNC_GETMODE) {
         status = getMode(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_SETMODE) {
+    } else if (cmd[0] == Report::ID::FSYNC_SETMODE) {
         status = setMode(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_GETFPS) {
+    } else if (cmd[0] == Report::ID::FSYNC_GETFPS) {
         status = getFps(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_SETFPS) {
+    } else if (cmd[0] == Report::ID::FSYNC_SETFPS) {
         status = setFps(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_SETDUTY) {
+    } else if (cmd[0] == Report::ID::FSYNC_GETDUTY) {
         status = getDuty(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_GETDUTY) {
+    } else if (cmd[0] == Report::ID::FSYNC_SETDUTY) {
         status = setDuty(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_SETPOLARITY) {
+    } else if (cmd[0] == Report::ID::FSYNC_GETPOLARITY) {
         status = getPolarity(cmd, response);
-    } else if(cmd[0] == Report::ID::FSYNC_GETPOLARITY) {
+    } else if (cmd[0] == Report::ID::FSYNC_SETPOLARITY) {
         status = setPolarity(cmd, response);
     }
 
@@ -54,122 +64,300 @@ CmdStatus FsyncController::task(uint8_t response[64])
     return status;
 }
 
-CmdStatus FsyncController::Init(uint8_t const *cmd, uint8_t response[64])
+CmdStatus FsyncController::init(uint8_t const *cmd, uint8_t response[64])
 {
+    unlockFsyncController();
+
+    FsyncI2cResult r = readReg(fsync_i2c, FSYNC_ADDRESS, PIN_CONFIG_VALID_REG);
+
+    if (r.status < 0 || r.data != 0)
+        return CmdStatus::NOK;
+
+    initok = true;
+
     return CmdStatus::OK;
 }
 
 /*
- * response[2] = fsync i2c bus id
- * response[3] = fsync i2c address
+ * response[2] = fsync i2c bus id (0xff - no stm found)
+ * response[3] = fsync i2c address (0xff - no stm found)
  * response[4:8] = fsync fw version (0xffffffff means controller is in bootloader mode (no firmware))
  */
 CmdStatus FsyncController::probe(uint8_t const *cmd, uint8_t response[64])
 {
-    uint8_t dummy = 0;
+    uint8_t dummy;
 
-    FsyncI2cResult r0 = readReg(i2c0, FSYNC_CONTROLLER_ADDRESS, 0x00);
-    response[2] = 0;
+    uint32_t neg1 = -1;
+    memcpy(response + 4, &neg1, sizeof(uint32_t));
 
-    *(int32_t *)(response + 4) = -1;
-
-    if (r0.status >= 0) {
-        response[3] = FSYNC_CONTROLLER_ADDRESS;
-        *(int32_t *)(response + 4) = r0.data; 
-        return CmdStatus::OK;
-    }
-
-    int b0 = i2c_read_timeout_us(
-        i2c0,
-        FSYNC_CONTROLLER_BOOT_ADDRESS,
-        &dummy,
-        1,
-        false,
-        1000
-    );
-
-    if (b0 != PICO_ERROR_GENERIC && b0 != PICO_ERROR_TIMEOUT) {
-        response[3] = FSYNC_CONTROLLER_BOOT_ADDRESS;
-        return CmdStatus::OK;
-    }
-
-    FsyncI2cResult r1 = readReg(i2c1, FSYNC_CONTROLLER_ADDRESS, 0x00);
+    FsyncI2cResult r1 = readReg(i2c1, FSYNC_ADDRESS, FW_VERSION_REG);
     response[2] = 1;
+    fsync_i2c = i2c1;
 
     if (r1.status >= 0) {
-        response[3] = FSYNC_CONTROLLER_ADDRESS;
-        *(int32_t *)(response + 4) = r1.data; 
+        response[3] = FSYNC_ADDRESS;
+        memcpy(response + 4, &r1.data, sizeof(r1.data));
         return CmdStatus::OK;
     }
 
     int b1 = i2c_read_timeout_us(
         i2c1,
-        FSYNC_CONTROLLER_BOOT_ADDRESS,
+        FSYNC_BOOT_ADDRESS,
         &dummy,
         1,
         false,
         1000
     );
 
-    if (b1 != PICO_ERROR_GENERIC && b1 != PICO_ERROR_TIMEOUT) {
-        response[3] = FSYNC_CONTROLLER_BOOT_ADDRESS;
+    if (b1 == 1) {
+        response[3] = FSYNC_BOOT_ADDRESS;
+        return CmdStatus::OK;
+    }
+
+    FsyncI2cResult r0 = readReg(i2c0, FSYNC_ADDRESS, FW_VERSION_REG);
+    response[2] = 0;
+    fsync_i2c = i2c0;
+
+    if (r0.status >= 0) {
+        response[3] = FSYNC_ADDRESS;
+        memcpy(response + 4, &r0.data, sizeof(r0.data));
+        return CmdStatus::OK;
+    }
+
+    int b0 = i2c_read_timeout_us(
+        i2c0,
+        FSYNC_BOOT_ADDRESS,
+        &dummy,
+        1,
+        false,
+        1000
+    );
+
+    if (b0 == 1) {
+        response[3] = FSYNC_BOOT_ADDRESS;
         return CmdStatus::OK;
     }
 
     response[2] = -1;
     response[3] = -1;
 
-    return CmdStatus::OK;
+    return CmdStatus::NOK;
 }
 
+/*
+ * cmd[1] = pin
+ * response[2:6] = pin capability
+ */
 CmdStatus FsyncController::getPinCapabilities(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    uint8_t pin; memcpy(&pin, cmd + 1, sizeof(pin));
+
+    if (pin >= PIN_CONFIG_REG_END - PIN_CONFIG_REG_START)
+        return CmdStatus::NOK;
+
+    FsyncI2cResult r = readReg(fsync_i2c, FSYNC_ADDRESS, PIN_CONFIG_REG_START + pin);
+    memcpy(response + 2, &r.data, sizeof(r.data));
+
+    return r.status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * cmd[1] = pin
+ * cmd[2:6] = pin capability
+ * response[2] = error code if NOK (1 - i2c, 2 - controller already initialised, 3 - invalid capability or pin)
+ */
 CmdStatus FsyncController::setPinCapabilities(uint8_t const *cmd, uint8_t response[64])
 {
+    if (initok) {
+        response[2] = 2;
+        return CmdStatus::NOK;
+    }
+
+    uint8_t pin; memcpy(&pin, cmd + 1, sizeof(pin));
+    uint32_t capability; memcpy(&capability, cmd + 2, sizeof(capability));
+
+    // prevent inputing invalid capabilities or multiple at once
+    if (capability & ~0b11111 || (capability & (capability - 1)) || capability == 0) {
+        response[2] = 3;
+        return CmdStatus::NOK;
+    }
+
+    if (pin >= PIN_CONFIG_REG_END - PIN_CONFIG_REG_START) {
+        response[2] = 3;
+        return CmdStatus::NOK;
+    }
+
+    int status = writeReg(fsync_i2c, FSYNC_ADDRESS, PIN_CONFIG_REG_START + pin, capability);
+
+    if (status < 0) {
+        response[2] = 1;
+        return CmdStatus::NOK;
+    }
+
     return CmdStatus::OK;
 }
 
+/*
+ * response[2:6] = config register
+ */
 CmdStatus FsyncController::getMode(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    FsyncI2cResult r = readReg(fsync_i2c, FSYNC_ADDRESS, CONFIG_REG);
+    memcpy(response + 2, &r.data, sizeof(r.data));
+
+    return r.status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * cmd[1:5] = config register
+ */
 CmdStatus FsyncController::setMode(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    uint32_t cfg; memcpy(&cfg, cmd + 1, sizeof(cfg));
+
+    if (cfg > 2)
+        return CmdStatus::NOK;
+
+    int status = writeReg(fsync_i2c, FSYNC_ADDRESS, CONFIG_REG, cfg);
+
+    return status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * response[2:6] = internal source frequency
+ * response[6:10] = actual output frequency
+ */
 CmdStatus FsyncController::getFps(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    FsyncI2cResult r = readReg(fsync_i2c, FSYNC_ADDRESS, INTERNAL_SOURCE_FREQ_REG);
+    memcpy(response + 2, &r.data, sizeof(r.data));
+
+    if (r.status < 0)
+        return CmdStatus::NOK;
+
+    r = readReg(fsync_i2c, FSYNC_ADDRESS, ACTUAL_OUTPUT_FREQ_REG);
+    memcpy(response + 6, &r.data, sizeof(r.data));
+
+    return r.status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * cmd[1:5] = requested frequency
+ */
 CmdStatus FsyncController::setFps(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    uint32_t fpsraw; memcpy(&fpsraw, cmd + 1, sizeof(fpsraw));
+
+    // make sure we are actually casting to a 32-bit float in the IEEE754 standard
+    static_assert(sizeof(float) == 4);
+    static_assert(std::numeric_limits<float>::is_iec559);
+    float fps; memcpy(&fps, cmd + 1, sizeof(fps));
+
+    if (fps > FSYNC_STM_MAX_FPS_ALLOWED || fps < FSYNC_STM_MIN_FPS_ALLOWED || !isfinite(fps))
+        return CmdStatus::NOK;
+
+    int status = writeReg(fsync_i2c, FSYNC_ADDRESS, INTERNAL_SOURCE_FREQ_REG, fpsraw);
+
+    return status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * cmd[1] = fsync channel
+ * response[2:6] = channel duty [0, 2048]
+ */
 CmdStatus FsyncController::getDuty(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    uint8_t chn; memcpy(&chn, cmd + 1, sizeof(chn));
+
+    if (chn >= (CHN_REGISTER_END - CHN_REGISTER_START) / 2)
+        return CmdStatus::NOK;
+
+    FsyncI2cResult r = readReg(fsync_i2c, FSYNC_ADDRESS, CHN_REGISTER_START + chn * REGS_PER_CHN);
+    memcpy(response + 2, &r.data, sizeof(r.data));
+
+    return r.status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * cmd[1] = fsync channel
+ * cmd[2:6] = channel duty [0, 2048]
+ */
 CmdStatus FsyncController::setDuty(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    uint8_t chn; memcpy(&chn, cmd + 1, sizeof(chn));
+    uint32_t duty; memcpy(&duty, cmd + 2, sizeof(duty));
+
+    if (chn >= (CHN_REGISTER_END - CHN_REGISTER_START) / 2)
+        return CmdStatus::NOK;
+
+    if (duty > DUTY_CYCLE_RANGE || duty < 0)
+        return CmdStatus::NOK;
+
+    int status = writeReg(fsync_i2c, FSYNC_ADDRESS, CHN_REGISTER_START + chn * REGS_PER_CHN, duty);
+
+    return status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * cmd[1] = fsync channel
+ * response[2:6] = channel polarity [0, 1]
+ */
 CmdStatus FsyncController::getPolarity(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    uint8_t chn; memcpy(&chn, cmd + 1, sizeof(chn));
+
+    if (chn >= (CHN_REGISTER_END - CHN_REGISTER_START) / 2)
+        return CmdStatus::NOK;
+
+    FsyncI2cResult r = readReg(fsync_i2c, FSYNC_ADDRESS, CHN_REGISTER_START + chn * REGS_PER_CHN + 1);
+    memcpy(response + 2, &r.data, sizeof(r.data));
+
+    return r.status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
 }
 
+/*
+ * cmd[1] = fsync channel
+ * cmd[2:6] = channel polarity [0, 1]
+ */
 CmdStatus FsyncController::setPolarity(uint8_t const *cmd, uint8_t response[64])
 {
-    return CmdStatus::OK;
+    uint8_t chn; memcpy(&chn, cmd + 1, sizeof(chn));
+    uint32_t polarity; memcpy(&polarity, cmd + 2, sizeof(polarity));
+
+    if (chn >= (CHN_REGISTER_END - CHN_REGISTER_START) / 2)
+        return CmdStatus::NOK;
+
+    if (polarity > 1 || polarity < 0)
+        return CmdStatus::NOK;
+
+    int status = writeReg(fsync_i2c, FSYNC_ADDRESS, CHN_REGISTER_START + chn * REGS_PER_CHN + 1, polarity);
+
+    return status >= 0 ? CmdStatus::OK : CmdStatus::NOK;
+}
+
+FsyncI2cResult FsyncController::readReg(i2c_inst_t *i2c, uint8_t addr, uint8_t reg)
+{
+    FsyncI2cResult r = {0, 0};
+
+    r.status |= i2c_write_timeout_us(
+        i2c,
+        addr,
+        &reg,
+        sizeof(reg),
+        true,
+        1000
+    );
+
+    r.status |= i2c_read_timeout_us(
+        i2c,
+        addr,
+        (uint8_t *)&r.data,
+        sizeof(r.data),
+        false,
+        1000
+    );
+
+    return r;
 }
 
 int FsyncController::writeReg(i2c_inst_t *i2c, uint8_t addr, uint8_t reg, uint32_t data)
@@ -197,27 +385,18 @@ int FsyncController::writeReg(i2c_inst_t *i2c, uint8_t addr, uint8_t reg, uint32
     return rc;
 }
 
-FsyncI2cResult FsyncController::readReg(i2c_inst_t *i2c, uint8_t addr, uint8_t reg)
+void FsyncController::unlockFsyncController()
 {
-    FsyncI2cResult r = {0, 0};
+    if (!locked)
+        return;
 
-    r.status |= i2c_write_timeout_us(
-        i2c,
-        addr,
-        &reg,
-        sizeof(r.status),
-        true,
-        1000
-    );
+    uint8_t dummy[64];
 
-    r.status |= i2c_read_timeout_us(
-        i2c,
-        addr,
-        (uint8_t *)&r.data,
-        sizeof(r.data),
-        false,
-        1000
-    );
+    if (probe(dummy, dummy) != CmdStatus::OK)
+        return;
 
-    return r;
+    int status = writeReg(fsync_i2c, FSYNC_ADDRESS, FW_VERSION_REG, I2C_WRITE_UNLOCK_MAGIC_NUMBER);
+
+    if (status >= 0)
+        locked = false; 
 }
--- firmware/source/interfaces/FsyncController.h
+++ firmware/source/interfaces/FsyncController.h
@@ -1,8 +1,119 @@
 #ifndef _INTERFACE_FSYNCCONTROLLER_H
 #define _INTERFACE_FSYNCCONTROLLER_H
 
-#define FSYNC_CONTROLLER_ADDRESS 0x12
-#define FSYNC_CONTROLLER_BOOT_ADDRESS 0x56
+#define FSYNC_ADDRESS 0x12
+#define FSYNC_BOOT_ADDRESS 0x56
+
+#define I2C_WRITE_UNLOCK_MAGIC_NUMBER 42
+
+#define FW_VERSION_REG 0
+#define CONFIG_REG 1
+#define INTERNAL_SOURCE_FREQ_REG 2
+#define ACTUAL_OUTPUT_FREQ_REG 3
+#define IN1_PRESENT_REG 4
+#define IN1_FREQUENCY_REG 5
+#define IN1_DUTY_CYCLE_REG 6
+#define CLOSEST_FREQUENCY_REG 7
+
+#define CHN_REGISTER_START	8 // = first register
+#define OUT1_DUTY_CYCLE_REG	8
+#define OUT1_POLARITY_REG	9
+#define OUT2_DUTY_CYCLE_REG	10
+#define OUT2_POLARITY_REG	11
+#define OUT3_DUTY_CYCLE_REG	12
+#define OUT3_POLARITY_REG	13
+#define OUT4_DUTY_CYCLE_REG	14
+#define OUT4_POLARITY_REG	15
+#define OUT5_DUTY_CYCLE_REG	16
+#define OUT5_POLARITY_REG	17
+#define OUT6_DUTY_CYCLE_REG	18
+#define OUT6_POLARITY_REG	19
+#define OUT7_DUTY_CYCLE_REG	20
+#define OUT7_POLARITY_REG	21
+#define OUT8_DUTY_CYCLE_REG	22
+#define OUT8_POLARITY_REG	23
+#define OUT9_DUTY_CYCLE_REG	24
+#define OUT9_POLARITY_REG	25
+#define OUT10_DUTY_CYCLE_REG	26
+#define OUT10_POLARITY_REG	27
+#define OUT11_DUTY_CYCLE_REG	28
+#define OUT11_POLARITY_REG	29
+#define OUT12_DUTY_CYCLE_REG	30
+#define OUT12_POLARITY_REG	31
+#define OUT13_DUTY_CYCLE_REG	32
+#define OUT13_POLARITY_REG	33
+
+#define CHN_REGISTER_END	34 // = last register + 1
+
+#define REGS_PER_CHN 2
+
+#define PIN_PA0_ID 0
+#define PIN_PA1_ID 1
+#define PIN_PA2_ID 2
+#define PIN_PA3_ID 3
+#define PIN_PA4_ID 4
+#define PIN_PA5_ID 5
+#define PIN_PA6_ID 6
+#define PIN_PA7_ID 7
+#define PIN_PA8_ID 8
+#define PIN_PA11_ID 9
+#define PIN_PA12_ID 10
+#define PIN_PA13_ID 11
+#define PIN_PA14_ID 12
+#define PIN_PA15_ID 13
+#define PIN_PB0_ID 14
+#define PIN_PB1_ID 15
+#define PIN_PB3_ID 16
+#define PIN_PB4_ID 17
+#define PIN_PB5_ID 18
+#define PIN_PB8_ID 19
+#define PIN_PC6_ID 20
+
+// Device Hardware Configuration Registers
+#define PIN_CONFIG_REG_START 34 // = first register
+#define PIN_PA0_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA0_ID
+#define PIN_PA1_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA1_ID
+#define PIN_PA2_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA2_ID
+#define PIN_PA3_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA3_ID
+#define PIN_PA4_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA4_ID
+#define PIN_PA5_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA5_ID
+#define PIN_PA6_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA6_ID
+#define PIN_PA7_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA7_ID
+#define PIN_PA8_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA8_ID
+#define PIN_PA11_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA11_ID
+#define PIN_PA12_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA12_ID
+#define PIN_PA13_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA13_ID
+#define PIN_PA14_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA14_ID
+#define PIN_PA15_CONFIG_REG PIN_CONFIG_REG_START + PIN_PA15_ID
+#define PIN_PB0_CONFIG_REG PIN_CONFIG_REG_START + PIN_PB0_ID
+#define PIN_PB1_CONFIG_REG PIN_CONFIG_REG_START + PIN_PB1_ID
+#define PIN_PB3_CONFIG_REG PIN_CONFIG_REG_START + PIN_PB3_ID
+#define PIN_PB4_CONFIG_REG PIN_CONFIG_REG_START + PIN_PB4_ID
+#define PIN_PB5_CONFIG_REG PIN_CONFIG_REG_START + PIN_PB5_ID
+#define PIN_PB8_CONFIG_REG PIN_CONFIG_REG_START + PIN_PB8_ID
+#define PIN_PC6_CONFIG_REG PIN_CONFIG_REG_START + PIN_PC6_ID
+
+// should be PIN_PC6_CONFIG_REG + 1
+#define PIN_CONFIG_REG_END 55
+
+#define PIN_CONFIG_VALID_REG 55
+#define ADC1_DATA_REG 56
+
+#define PIN_CONFIG_TYPE_HIGH_Z_Pos 0
+#define PIN_CONFIG_TYPE_HIGH_Z (1 << PIN_CONFIG_TYPE_HIGH_Z_Pos)
+#define PIN_CONFIG_TYPE_PWM_Pos 1
+#define PIN_CONFIG_TYPE_PWM (1 << PIN_CONFIG_TYPE_PWM_Pos)
+#define PIN_CONFIG_TYPE_ADC_Pos 2
+#define PIN_CONFIG_TYPE_ADC (1 << PIN_CONFIG_TYPE_ADC_Pos)
+#define PIN_CONFIG_TYPE_PWM_KEEPAWAKE_Pos 3
+#define PIN_CONFIG_TYPE_PWM_KEEPAWAKE (1 << PIN_CONFIG_TYPE_PWM_KEEPAWAKE_Pos)
+#define PIN_CONFIG_TYPE_PWM_HFSTROBE_Pos 4
+#define PIN_CONFIG_TYPE_PWM_HFSTROBE (1 << PIN_CONFIG_TYPE_PWM_HFSTROBE_Pos)
+
+#define FSYNC_STM_MIN_FPS_ALLOWED   0.1
+#define FSYNC_STM_MAX_FPS_ALLOWED   600.0
+
+#define DUTY_CYCLE_RANGE 2048
 
 #include "PicoInterfacesBoard.h"
 #include "BaseInterface.h"
@@ -22,7 +133,7 @@ public:
     CmdStatus process(uint8_t const *cmd, uint8_t response[64]);
     CmdStatus task(uint8_t response[64]);
 
-    CmdStatus Init(uint8_t const *cmd, uint8_t response[64]);
+    CmdStatus init(uint8_t const *cmd, uint8_t response[64]);
     CmdStatus probe(uint8_t const *cmd, uint8_t response[64]);
     CmdStatus getPinCapabilities(uint8_t const *cmd, uint8_t response[64]);
     CmdStatus setPinCapabilities(uint8_t const *cmd, uint8_t response[64]);
@@ -37,8 +148,14 @@ public:
     CmdStatus setPolarity(uint8_t const *cmd, uint8_t response[64]);
 
 private:
-    int writeReg(i2c_inst_t *i2c, uint8_t addr, uint8_t reg, uint32_t data);
     FsyncI2cResult readReg(i2c_inst_t *i2c, uint8_t addr, uint8_t reg);
+    int writeReg(i2c_inst_t *i2c, uint8_t addr, uint8_t reg, uint32_t data);
+    void unlockFsyncController();
+
+    bool initok = false;
+    bool locked = true;
+
+    i2c_inst_t *fsync_i2c = NULL;
 };
 
 #endif
